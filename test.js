4fd085d6c50b493eade30eeffafb3a44abaa4c49
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-15 09:19:51 +0000

    Require Node.js 8, update dependencies, add TypeScript definition (#1)

--- test.js
+++ test.js
@@ -1,28 +1,28 @@
 import test from 'ava';
-import m from '.';
+import pEvery from '.';
 
 test('true', async t => {
-	t.true(await m([Promise.resolve(1), 3], x => x % 2));
-	t.true(await m([1, 3], x => Promise.resolve(x % 2)));
+	t.true(await pEvery([Promise.resolve(1), 3], x => x % 2));
+	t.true(await pEvery([1, 3], async value => value % 2));
 });
 
 test('false', async t => {
-	t.false(await m([Promise.resolve(1), 2, 3, 4], x => x % 2));
-	t.false(await m([1, 2, 3, 4], x => Promise.resolve(x % 2)));
+	t.false(await pEvery([Promise.resolve(1), 2, 3, 4], x => x % 2));
+	t.false(await pEvery([1, 2, 3, 4], async value => value % 2));
 });
 
 test('fail fast', async t => {
 	const concurrency = 1;
 	let count = 0;
 
-	await m([Promise.resolve(1), 2, 3, 4], x => {
+	await pEvery([Promise.resolve(1), 2, 3, 4], value => {
 		count++;
-		return x % 2;
+		return value % 2;
 	}, {concurrency});
 
 	t.is(count, 2);
 });
 
 test('handles empty iterable', async t => {
-	t.deepEqual(await m([]), true);
+	t.deepEqual(await pEvery([]), true);
 });
